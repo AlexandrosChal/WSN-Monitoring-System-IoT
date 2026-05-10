dbbd9adcc049a092ab68eb2bb01550609e72b4e2
AlexandrosCh <122706408+AlexandrosChal@users.noreply.github.com>
2026-05-10 20:16:14 +0300

    Τελικος κωδικας

--- Shared folder/app.py
+++ Shared folder/app.py
@@ -3,23 +3,23 @@ import pymongo
 
 app = Flask(__name__)
 
-# Σύνδεση με MongoDB (όπως ακριβώς κάναμε στο logger)
+# Σύνδεση με MongoDB
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["wsn_project"]
 collection = db["sensor_data"]
 
 @app.route('/')
 def index():
-    # Επιστρέφει την κεντρική σελίδα (το γραφικό περιβάλλον)
+    
     return render_template('index.html')
 
 @app.route('/api/data')
 def get_data():
-    # Τραβάει τις 30 πιο πρόσφατες μετρήσεις (Ιστορικά/Real-time)
+    # Παίρνει τις 30 πιο πρόσφατες μετρήσεις
     cursor = collection.find({}, {"_id": 0}).sort("Timestamp", -1).limit(30)
     data = list(cursor)
     
-    # Αντιστροφή της λίστας για να εμφανίζονται χρονολογικά στο γράφημα (αριστερά τα παλιά, δεξιά τα νέα)
+    # Αντιστροφή της λίστας για να εμφανίζονται χρονολογικά στο γράφημα
     data.reverse() 
     return jsonify(data)
 
